54b4e5aa5563f7e251cac901163b346a9aba2ad5
Owen Muchini <owenmuchini1@gmail.com>
2023-04-09 15:47:30 +0930

    changes to heroImageSection

--- src/components/HeroImageContainer.jsx
+++ src/components/HeroImageContainer.jsx
@@ -4,8 +4,8 @@ function HeroImageContainer() {
     const [idx, setIdx] = useState(1);
     const maxImgs = 4;
 
-    const disabledBtn = 'bg-[#79797950]';
-    const enabledBtn = 'bg-[#00000050]';
+    const disabledBtn = 'bg-[#7979790]';
+    const enabledBtn = 'bg-[#00000080]';
 
     const handlePrev = () => {
       setIdx(idx => idx-1);
@@ -18,21 +18,25 @@ function HeroImageContainer() {
     };
 
     return (
-        <div className="w-full h-full relative flex items-center overflow-clip">
+        <div className="w-full h-full relative flex items-center overflow-clip ">
             <div className="w-1/12 h-full flex items-center">
                 <div className={`h-full md:h-4/5 w-full ${idx === 0 && 'invisible'}`}>
                     {idx > 0 &&
-                        <img src={`/lookbook/0/${idx-1}.jpg`} alt="" className="w-full h-full object-cover" />
+                        <div className="h-full flex items-center" >
+                            <div className="h-fit cursor-pointer" onClick={handlePrev}>
+                                <img src={`/lookbook/0/${idx-1}.jpg`} alt="" className="object-cover w-full h-full blur-[1px] hover:blur-none transition-[filter] duration-150" />
+                            </div>
+                        </div> 
                     }
                 </div>
             </div>
             <div className="w-1/12 h-full flex justify-center items-center">
                 <button
-                    className={`w-full aspect-square rounded-full ${idx === 0 ? disabledBtn : enabledBtn}`}
+                    className={`${idx === 0 ? disabledBtn : enabledBtn} drop-shadow hover:drop-shadow-[1px_1px_5px_rgba(0,0,0,0.5)] w-full aspect-square rounded-full bg-cover bg-[url('leftArrow.png')] `}
                     disabled={idx === 0}
                     onClick={handlePrev}
                 >
-                    {"<"}
+                    {""}
                 </button>
             </div>
             <div className="w-8/12 h-full flex items-center">
@@ -42,17 +46,22 @@ function HeroImageContainer() {
             </div>
             <div className="w-1/12 h-full flex justify-center items-center">
                 <button
-                    className={`w-full aspect-square rounded-full ${idx === maxImgs ? disabledBtn : enabledBtn}`}
+                    className={`${idx === maxImgs ? disabledBtn : enabledBtn} drop-shadow hover:drop-shadow-[1px_1px_5px_rgba(0,0,0,0.5)] w-full aspect-square rounded-full bg-cover bg-[url('rightArrow.png')] `}
                     disabled={idx === maxImgs}
                     onClick={handleNext}
                 >
-                    {">"}
+                    {""}
                 </button>
             </div>
             <div className="w-1/12 h-full flex items-center">
                 <div className={`h-full md:h-4/5 w-full ${idx === maxImgs && 'invisible'}`}>
                     {idx <= maxImgs &&
-                        <img src={`/lookbook/0/${idx+1}.jpg`} alt="" className="w-full h-full object-cover" />
+                        <div className="h-full flex items-center" >
+                            <div className="h-fit cursor-pointer" onClick={handleNext}>
+                            <img src={`/lookbook/0/${idx+1}.jpg`} alt="" className="object-contain w-full h-full blur-[1px] hover:blur-none transition-[filter] duration-150" />
+                            </div>
+                        </div> 
+                        
                     }
                 </div>
             </div>
